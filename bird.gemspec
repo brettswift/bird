1109843375a3e8cdef30c3575489daf812fc11fa
Brett Swift <brettswift@gmail.com>
2014-01-13 14:48:46 -0700

    setup overrides complete

--- bird.gemspec
+++ bird.gemspec
@@ -15,7 +15,7 @@ Gem::Specification.new do |s|
   s.license     = "MIT"
 
   # s.rubyforge_project = "bird"
-
+  #IMPORTANT - the git commands below only work if your changes are staged. :)
   s.files         = `git ls-files`.split("\n")
   s.test_files    = `git ls-files -- {test,spec,features}/*`.split("\n")
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
--- lib/bird/cli/setup.rb
+++ lib/bird/cli/setup.rb
@@ -5,25 +5,19 @@ require 'bird/config/configuration'
 
 module Bird
   class Setup < ThorBase
-
-    # desc "setup", "setup stuff"
-    # option :setup, :required => false, :banner => " help"
-    # def help(command, options={:default => nil})
-    #   ok "here is setup "
-    # end
-
+ 
     desc "override", "overrides or creates values for stored configuration"
-    option :host, :required => false, :banner => "vcloud host"
-    option :org_name, :required => false, :banner => "vcloud organization name"
-    option :user, :required => false, :banner => "vcloud user"
-    option :pass_encrypted, :required => false, :banner => "vcloud password (encrypted) overrides pass"
-    option :pass, :required => false, :banner => "vcloud password (plain text) overridden by pass_encrypted"
+    option :hostname,       :required => false, :aliases => '-n', :banner => "vcloud host"
+    option :org_name,       :required => false, :aliases => '-o',  :banner => "vcloud organization name"
+    option :user,           :required => false, :aliases => '-u', :banner => "vcloud user"
+    option :passEncrypted,  :required => false, :banner => "  !!!BUG!!!: use underscore, not dash /BUG  vcloud password (encrypted) overrides pass"
+    option :pass,           :required => false, :banner => "vcloud password (plain text) overridden by pass_encrypted"
     def override
       config = Bird::Configuration.new
-      config.host = options[:host] if options[:host]
+      config.host = options[:hostname] if options[:hostname]
       config.org_name = options[:org_name] if options[:org_name]
       config.user = options[:user] if options[:user]
-      config.pass_encrypted = options[:pass] if options[:pass_encrypted]
+      config.pass_encrypted = options[:pass_encrypted] if options[:pass_encrypted]
       config.pass = options[:pass] if options[:pass]
       config.save
     end
